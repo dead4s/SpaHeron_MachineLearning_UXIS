c19315a6358df2e762049a03a64133b49ef0b0a9
HYORIMLEE <37135345+HYORIMLEE@users.noreply.github.com>
2021-06-24 15:18:17 +0900

    enum add, header edit, add clock

--- student.cpp
+++ student.cpp
@@ -14,16 +14,17 @@ bool Student::isFixed(){
     else return false;
 }
 
-int Student::change_state(){
-    if(FACE_UNDETECTED_THES<=cnt_face.first)   state = 0;
-    else if(EYE_UNFIXED_THES<=cnt_eye.first)   state = 3;
-    else if(EYE_FIXED_THES<=cnt_eye.second)    state = 2;
+State Student::change_state(){
+    if(FACE_UNDETECTED_THES<=cnt_face.first)   state = ABSCENT;
+    else if(EYE_FIXED_THES<=cnt_eye.first)   state = ATTEND;
+    else if(state !=ABSCENT&&EYE_UNFIXED_THES<=cnt_eye.first)   state = DISTRACT;
+    else if(state !=ABSCENT&&EYE_FIXED_THES<=cnt_eye.second)    state = CONCENTRATE;
     cnt_face={0,0};
     cnt_eye={0,0};
-
+    cout<<"change state"<<endl;
     return state;
 }
-int Student::chk_state(pair<int,int> pupil){
+State Student::chk_state(pair<int,int> pupil){
     if(pupil.first<0&&pupil.second<0) {
         cnt_face.first++;
         cnt_eye.first++;
@@ -37,5 +38,6 @@ int Student::chk_state(pair<int,int> pupil){
     else{
         cnt_eye.first++;
     }
+    cout<<"check state"<<endl;
     return state;
 }
--- student.h
+++ student.h
@@ -1,15 +1,16 @@
 #ifndef STUDENT_H
 #define STUDENT_H
+#include <iostream>
+using namespace std;
+enum State{
+    ATTEND,
+    ABSCENT,
+    CONCENTRATE,
+    DISTRACT,
+};
 
-const int STATE = 0;
-const int ATTEND = 1;
-const int ABSCENT = 0;
-const int CONCENTRATE = 2;
-const int DISTRACT = 3;
-string STATES_STR[] = {"ATTEND","ABSCENT","CONCENTRATE","DISTRACT"};
 
 const int upper_bound = 10;
-
 const int FACE_UNDETECTED_THES = 10;
 const int EYE_FIXED_THES = 10;
 const int EYE_UNFIXED_THES = 10;
@@ -21,20 +22,19 @@ const int EYE_UNFIXED_CNT = 0;
 pair<int,int> pupil;
 
 class Student{
-    public:
-    int state;
+public:
+    State state;
     pair<int,int> cnt_face,cnt_eye;//first = there is'nt, second = there is
     pair<int,int> ex_pupil;
     long long id=-1;
 
     Student(){
-        state = 1;
+        State state = ATTEND;
         cnt_face = {0,0};
         cnt_eye = {0,0};
     }
     bool isFixed();
-    int change_state();
-    int chk_state(pair<int,int> pupil);
-
+    State change_state();
+    State chk_state(pair<int,int> pupil);
 };
 #endif
